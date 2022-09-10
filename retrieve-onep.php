b0f77611c714f862548bce6d812605d312da7b9b
MISS <basilio.stephanie@auf.edu.ph>
2022-09-10 14:45:42 +0800

    second commit

--- retrieve-onep.php
+++ retrieve-onep.php
@@ -10,15 +10,19 @@ $product = $stripe->products->retrieve(
 	[]
 );
 $price = $stripe->prices->retrieve('price_1LgJlKH3YSRNu0EdlwlgGu8C',[]);
-echo '<pre>';
-var_dump($product);
-var_dump($price);
-echo '</pre>';
+#echo '<pre>';
+#var_dump($product);
+#var_dump($price);
+#echo '</pre>';
 ?><!DOCTYPE html>
 <html>
   <head>
     <title>Buy</title>
     <link rel="stylesheet" href="style.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
     <script src="https://js.stripe.com/v3/"></script>
   </head>
